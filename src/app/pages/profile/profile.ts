250f00aa10445aabc55b7b0525336c0572f04374
JuanAntonio2025 <jasanchezdec@gmail.com>
2026-02-02 09:44:58 +0100

    Creados directorios Auth y Pages

---
+++ src/app/pages/profile/profile.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  imports: [],
+  templateUrl: './profile.html',
+  styleUrl: './profile.css',
+})
+export class Profile {
+
+}
+
+export class ProfileComponent {
+}
---
+++ src/app/pages/register/register.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-register',
+  imports: [],
+  templateUrl: './register.html',
+  styleUrl: './register.css',
+})
+export class Register {
+
+}
+
+export class RegisterComponent {
+}
